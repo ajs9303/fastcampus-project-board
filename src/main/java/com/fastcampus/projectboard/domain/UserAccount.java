6263284126e3201b860af776ec059dd279dcff5d
Sun <ajs9303@nave.com>
2024-11-14 12:44:09 +0900

    #61 - 엔티티 equals(), hashcode() 비교 로직에 getter 적용
    여기서 필드에 직접 접근하면,
    하이버네이트가 지연 로딩하려고 만든 프록시 객체를 다룰 때
    필드 값이 'null' 일 수 있음
    그러면 제대로 비교 로직을 수행할 수 없기 때문에
    getter를 이용함.
    이것으로 프록시 객체를 타더라도 제대로 값을 읽어올 수 있음

--- src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
+++ src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
@@ -20,7 +20,6 @@ public class UserAccount extends AuditingFields {
     @Column(length = 50)
     private String userId;
     @Setter @Column(nullable = false) private String userPassword;
-
     @Setter @Column(length = 100) private String email;
     @Setter @Column(length = 100) private String nickname;
     @Setter private String memo;
